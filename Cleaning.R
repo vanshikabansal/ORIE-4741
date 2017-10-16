164cd79d4319412c397c76ba58a3f1342d9a8e26
austonli <al884@cornell.edu>
2017-10-16 18:43:10 -0400

    cleaning script in R

---
+++ Cleaning.R
@@ -0,0 +1,4 @@
+setwd("C:/Users/meaus/OneDrive/Junior/1st Semester/ORIE 4741/hong-kong-horse-racing") #setting the working directory
+
+horse<- read.csv("race-result-horse.csv")
+race <- read.csv("race-result-race.csv")
