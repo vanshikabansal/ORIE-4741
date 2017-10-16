5fb60d75e8a34ec024dd6c9689ef3607023b460f
austonli <al884@cornell.edu>
2017-10-16 19:35:59 -0400

    completed script, takes a long time tho lmao

--- Cleaning.R
+++ Cleaning.R
@@ -9,12 +9,31 @@ names(race)
 n <- nrow(horse)
 
 rr <- horse$horse_number
-marks <- list()
+marks <- c()
 for (k in 1:n){
   #if the line is character and the next line is a 1, then that completes a race
   if (k!= n && !is.na(rr[k+1])){
     if (rr[k+1]==1){
-      marks = c(marks,k)
+      marks <- c(marks,k)
     }
   }
+}
+
+marks <- c(marks,n)
+
+jk <- unique(horse$jockey)
+tr <- unique(horse$trainer)
+empty1 <- rep(0,length((jk)))
+empty2 <- rep(0,length((tr)))
+jockey <- data.frame(jk,empty1)
+trainer <- data.frame(tr,empty2)
+for (i in 1:length(jk)){
+  count <- 0
+  for (j in 1:n){
+    if (horse$jockey[k] == jk[i]){
+      count <- count + 1
+      jockey[i,2] <- rr[j]
+    }
+  }
+  jockey[i,2] <- jockey[i,2]/count
 }
