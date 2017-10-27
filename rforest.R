7a0f19a86d6002c4ede142296925fc9cb1752242
vanshikabansal <vanshikabansal@users.noreply.github.com>
2017-10-27 13:57:21 -0400

    Add files via upload

--- rforest.R
+++ rforest.R
@@ -6,49 +6,65 @@ race <- read.csv("race-result-race.csv", stringsAsFactors = FALSE)
 
 # Merge race and horse information
 mydata <- merge(horse, race, by = "race_id")
+mydata <- mydata[!is.na(mydata$finishing_position), ]
 
 #separate races by distance
 race_lengths <- unique(mydata$race_distance)
 race_lengths <- sort(race_lengths)
 
 #Removing columns deemed unnecessary
-mydata <- subset(mydata, select = -c(horse_name, src, sectional_time, incident_report))
+mydata <- subset(mydata, select = -c(horse_id, src, sectional_time, incident_report, length_behind_winner, horse_number, race_date, race_number))
 
 #Cleaning positions to remove ties
-mydata[mydata$finishing_position == "1 DH" & !is.na(mydata$finishing_position),] = 1
-mydata[mydata$finishing_position == "2 DH" & !is.na(mydata$finishing_position),] = 2
-mydata[mydata$finishing_position == "3 DH" & !is.na(mydata$finishing_position),] = 3
-mydata[mydata$finishing_position == "4 DH" & !is.na(mydata$finishing_position),] = 4
-mydata[mydata$finishing_position == "5 DH" & !is.na(mydata$finishing_position),] = 5
-mydata[mydata$finishing_position == "6 DH" & !is.na(mydata$finishing_position),] = 6
-mydata[mydata$finishing_position == "7 DH" & !is.na(mydata$finishing_position),] = 7
-mydata[mydata$finishing_position == "8 DH" & !is.na(mydata$finishing_position),] = 8
-mydata[mydata$finishing_position == "9 DH" & !is.na(mydata$finishing_position),] = 9
-mydata[mydata$finishing_position == "10 DH" & !is.na(mydata$finishing_position),] = 10
-mydata[mydata$finishing_position == "11 DH" & !is.na(mydata$finishing_position),] = 11
-mydata[mydata$finishing_position == "12 DH" & !is.na(mydata$finishing_position),] = 12
-mydata[mydata$finishing_position == "13 DH" & !is.na(mydata$finishing_position),] = 13
-mydata[mydata$finishing_position == "14 DH" & !is.na(mydata$finishing_position),] = 14
+mydata[mydata$finishing_position == "1 DH",] = 1
+mydata[mydata$finishing_position == "2 DH",] = 2
+mydata[mydata$finishing_position == "3 DH",] = 3
+mydata[mydata$finishing_position == "4 DH" ,] = 4
+mydata[mydata$finishing_position == "5 DH",] = 5
+mydata[mydata$finishing_position == "6 DH",] = 6
+mydata[mydata$finishing_position == "7 DH",] = 7
+mydata[mydata$finishing_position == "8 DH",] = 8
+mydata[mydata$finishing_position == "9 DH",] = 9
+mydata[mydata$finishing_position == "10 DH",] = 10
+mydata[mydata$finishing_position == "11 DH",] = 11
+mydata[mydata$finishing_position == "12 DH",] = 12
+mydata[mydata$finishing_position == "13 DH",] = 13
+mydata[mydata$finishing_position == "14 DH",] = 14
 
-non_placing_horses <-mydata[mydata$finishing_position == "WV-A" |mydata$finishing_position== "WV"  | mydata$finishing_position=="WX"  | mydata$finishing_position=="PU" | mydata$finishing_position== "UR" |  mydata$finishing_position=="FE"  | mydata$finishing_position=="TNP" |mydata$finishing_position== "DISQ" | mydata$finishing_position=="WX-A" | mydata$finishing_position=="DNF" ,]
-placing_horses <- mydata[!(mydata$finishing_position %in%  non_placing_horses$finishing_position),]
+non_placing_horses <- mydata[mydata$finishing_position == "WV-A" |mydata$finishing_position== "WV"  | mydata$finishing_position=="WX"  | mydata$finishing_position=="PU" | mydata$finishing_position== "UR" |  mydata$finishing_position=="FE"  | mydata$finishing_position=="TNP" |mydata$finishing_position== "DISQ" | mydata$finishing_position=="WX-A" | mydata$finishing_position=="DNF",]
+withdrawn_horses <- mydata[mydata$finishing_position == "WV-A" |mydata$finishing_position== "WV"  | mydata$finishing_position=="WX"  | mydata$finishing_position=="WX-A" ,]
 
+#Use non withdrawn horses for predictions
+mydata <- mydata[!(mydata$finishing_position %in%  withdrawn_horses$finishing_position),]
 
-#mydata[,c("finishing_position", "actual_weight", "declared_horse_weight", "draw")] <- as.integer(mydata[,c("finishing_position", "actual_weight", "declared_horse_weight", "draw")])
+#Allotting non placeing horses that weren't withdrawn the last or a bad position of 14
+mydata$finishing_position <- as.numeric(mydata$finishing_position)
+mydata[is.na(mydata$finishing_position) & is.na(mydata$running_position_1), "running_position_1"] = 14
+mydata[is.na(mydata$finishing_position) & is.na(mydata$running_position_2), "running_position_2"] = 14
+mydata[is.na(mydata$finishing_position) & is.na(mydata$running_position_3), "running_position_3"] = 14
+mydata[is.na(mydata$finishing_position) & is.na(mydata$running_position_4), "running_position_4"] = 14
+mydata[is.na(mydata$finishing_position) & is.na(mydata$running_position_5), "running_position_5"] = 14
+mydata[is.na(mydata$finishing_position) & is.na(mydata$running_position_6), "running_position_6"] = 14
 
+mydata$finishing_position[is.na(as.numeric(mydata$finishing_position))] = 14
 
-# If no finishing position, we assume the horse was disqualified and allot it the last pos
+#Convert data to numeric to run random forest
+mydata$actual_weight <- as.numeric(mydata$actual_weight)
+mydata$declared_horse_weight <- as.numeric(mydata$declared_horse_weight)
+mydata$draw <- as.numeric(mydata$draw)
+mydata$finish_time <- as.numeric(mydata$finish_time)
+mydata$win_odds <- as.numeric(mydata$finish_time)
 
-
-race1 <- mydata[mydata$race_distance == race_lengths[1] & !is.na(mydata$race_distance),]
-race2 <- mydata[mydata$race_distance  == race_lengths[2] & !is.na(mydata$race_distance),]
-race3 <- mydata[mydata$race_distance == race_lengths[3] & !is.na(mydata$race_distance),]
-race4 <- mydata[mydata$race_distance == race_lengths[4] & !is.na(mydata$race_distance),]
-race5 <- mydata[mydata$race_distance == race_lengths[5] & !is.na(mydata$race_distance),]
-race6 <- mydata[mydata$race_distance == race_lengths[6] & !is.na(mydata$race_distance),]
-race7 <- mydata[mydata$race_distance == race_lengths[7] & !is.na(mydata$race_distance),]
-race8 <- mydata[mydata$race_distance == race_lengths[8] & !is.na(mydata$race_distance),]
-race9 <- mydata[mydata$race_distance == race_lengths[9] & !is.na(mydata$race_distance),]
+#Separate races by distance
+race1 <- mydata[mydata$race_distance == race_lengths[1],]
+race2 <- mydata[mydata$race_distance  == race_lengths[2],]
+race3 <- mydata[mydata$race_distance == race_lengths[3],]
+race4 <- mydata[mydata$race_distance == race_lengths[4],]
+race5 <- mydata[mydata$race_distance == race_lengths[5],]
+race6 <- mydata[mydata$race_distance == race_lengths[6],]
+race7 <- mydata[mydata$race_distance == race_lengths[7],]
+race8 <- mydata[mydata$race_distance == race_lengths[8],]
+race9 <- mydata[mydata$race_distance == race_lengths[9],]
 
 race_frames <- c(race1, race2, race3, race4, race5, race6, race7, race8, race9)
 
